2c0ead27d93e89f35a99ed6a9d6efc538c0c5d45
PAPPU MOHANA KRISHNA <163747615+Mohanpappu@users.noreply.github.com>
2025-11-05 21:44:49 +0530

    Update UserRepository.java

--- src/main/java/com/klu/ecommerce/repository/UserRepository.java
+++ src/main/java/com/klu/ecommerce/repository/UserRepository.java
@@ -1,12 +1,10 @@
 package com.klu.ecommerce.repository;
 
-import com.klu.ecommerce.entity.User;
+import com.klu.ecommerce.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
-    Optional<User> findByEmail(String email);
 }
