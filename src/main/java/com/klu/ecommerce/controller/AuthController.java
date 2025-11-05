e8926b74877c27eb21330b7823e6b0f0fe2c782a
PAPPU MOHANA KRISHNA <163747615+Mohanpappu@users.noreply.github.com>
2025-11-05 21:51:36 +0530

    Update AuthController.java

--- src/main/java/com/klu/ecommerce/controller/AuthController.java
+++ src/main/java/com/klu/ecommerce/controller/AuthController.java
@@ -1,56 +1,31 @@
 package com.klu.ecommerce.controller;
 
 import com.klu.ecommerce.model.User;
-import com.klu.ecommerce.repository.UserRepository;
-import com.klu.ecommerce.security.JwtUtil;
+import com.klu.ecommerce.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/back1/api/auth")
 public class AuthController {
 
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final AuthenticationManager authenticationManager;
-    private final JwtUtil jwtUtil;
+    private final UserService userService;
 
-    public AuthController(UserRepository userRepository, PasswordEncoder passwordEncoder,
-                          AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-        this.authenticationManager = authenticationManager;
-        this.jwtUtil = jwtUtil;
+    public AuthController(UserService userService) {
+        this.userService = userService;
     }
 
     @PostMapping("/signup")
     public ResponseEntity<Map<String,String>> signup(@RequestBody Map<String,String> userMap) {
-        User user = new User();
-        user.setUsername(userMap.get("username"));
-        user.setEmail(userMap.get("email"));
-        user.setPassword(passwordEncoder.encode(userMap.get("password")));
-        userRepository.save(user);
-
-        Map<String,String> response = new HashMap<>();
-        response.put("message", "User registered successfully!");
-        return ResponseEntity.ok(response);
+        String message = userService.registerUser(userMap.get("username"), userMap.get("email"), userMap.get("password"));
+        return ResponseEntity.ok(Map.of("message", message));
     }
 
     @PostMapping("/login")
     public ResponseEntity<Map<String,String>> login(@RequestBody Map<String,String> userMap) {
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(userMap.get("username"), userMap.get("password"))
-        );
-
-        String token = jwtUtil.generateToken(userMap.get("username"));
-        Map<String,String> response = new HashMap<>();
-        response.put("token", token);
-        return ResponseEntity.ok(response);
+        String token = userService.loginUser(userMap.get("username"), userMap.get("password"));
+        return ResponseEntity.ok(Map.of("token", token));
     }
 }
