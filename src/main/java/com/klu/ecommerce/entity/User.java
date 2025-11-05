a9b98a5678438bf6eed1540c0d31738d8421d87b
PAPPU MOHANA KRISHNA <163747615+Mohanpappu@users.noreply.github.com>
2025-11-05 21:52:00 +0530

    Update User.java

--- src/main/java/com/klu/ecommerce/entity/User.java
+++ src/main/java/com/klu/ecommerce/entity/User.java
@@ -8,8 +8,13 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true)
     private String username;
+
+    @Column(unique = true)
     private String email;
+
     private String password;
 
     // getters and setters
